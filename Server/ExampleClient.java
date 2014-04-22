246c690bc1b8e5ce7e6a506809b0ac9e24d733fb
jethva <jethva@purdue.edu>
2014-04-22 02:35:48 -0400

    update

--- Server/ExampleClient.java
+++ Server/ExampleClient.java
@@ -29,12 +29,12 @@ OutputStream out = socket.getOutputStream();
 // now actually write the file
 int count=0;
 byte[] buffer = new byte[1024];
-File file = new File("test.jpg");
-FileInputStream in = new FileInputStream("test.jpg");
+File file = new File("test2.jpg");
+FileInputStream in = new FileInputStream("test2.jpg");
 
 long length = file.length();
 
-String x = "{\"option\":\"photo_upload\",\"file_name\":\"test.jpg\",\"length\":"+length+",\"email\":\"email@gmail.com\"}\n";
+String x = "{\"option\":\"photo_upload\",\"file_name\":\"test2.jpg\",\"length\":"+length+",\"email\":\"email@gmail.com\"}\n";
 pw.println(x);
 
 
